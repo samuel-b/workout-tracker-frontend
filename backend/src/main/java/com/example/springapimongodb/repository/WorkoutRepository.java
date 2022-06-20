27ce0291c6b0e9d5f7ece68e2fe7dd31cb52f7d5
Sam-B-Hub <64763981+Sam-B-Hub@users.noreply.github.com>
2022-06-20 14:41:14 +1000

    re-pushing previous commit at root level

--- backend/src/main/java/com/example/springapimongodb/repository/WorkoutRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springapimongodb.repository;
-
-import com.example.springapimongodb.modal.Workout;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface WorkoutRepository extends MongoRepository<Workout, String> {
-}
